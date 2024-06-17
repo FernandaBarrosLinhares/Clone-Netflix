72cafaaf140a1b17100de222107d1fbc259271fd
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-06-17 20:14:22 -0300

    Links midias sociais

---
+++ main.js
@@ -0,0 +1,20 @@
+// document.addEventListener('DomContentLoaded', ()=>{
+//     const instagramLink = document.getElementById('intagram').parentElement;
+//     const facebookLink = document.getElementById('facebook').parentElement;
+//     const twitterLink = document.getElementById('twitter').parentElement;
+
+//     instagramLink.addEventListener('click', (event)=>{
+//         event.preventDefault();
+//         console.log('Instagram link clicked')
+//     });
+
+//     facebookLink.addEventListener('click', (event)=>{
+//         event.preventDefault();
+//         console.log('Facebook link clicked')
+//     })
+//     twitterLink.addEventListener('click', (event)=>{
+//         event.preventDefault();
+//         console.log('Twitter link clicked')
+//     })
+
+// })
